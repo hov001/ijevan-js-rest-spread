a57a0b26c80d33890a78d03f7665c10566570597
Hovhannes Kocharyan <hovkocharyan81@gmail.com>
2023-03-03 20:09:27 +0400

    feat(lesson.js): add rest and spread operators

---
+++ lesson.js
@@ -0,0 +1,53 @@
+// Rest
+
+// function foo(first, second, third, ...fooItems) {
+//   console.log("first:::", first);
+//   console.log("args:::", fooItems);
+
+//   return fooItems.map((fooItem) => fooItem.length);
+// }
+
+// const res = foo("hello", "world", "some", "another", "vuuuy");
+
+// console.log(res);
+
+// Spread
+
+// const person = {
+//   name: "Hovhannes",
+//   age: 26,
+// };
+
+// console.log("person:::", person);
+
+// const personData = {
+//   samson: 1,
+//   ...person,
+//   stomach: [],
+// };
+
+// for (let key in person) {
+//   personData[key] = person[key];
+// }
+
+// personData.stomach = [];
+
+// console.log("personData:::", personData);
+
+// const arr = [1, 2, 3];
+// const arr2 = [4, 5, 6];
+// const arr3 = [7, 8, [1, 2, 9]];
+
+// const arr4 = [...arr, ...arr2, ...arr3];
+
+// console.log(arr4);
+
+// arr3[2].push(6);
+
+// console.log(arr4);
+
+// const str = "hello";
+
+// const arr = { ...str };
+
+// console.log(arr);
